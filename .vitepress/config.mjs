48089155bf812046dcb27ff7200a28ec1eebf949
wxhzh <mygod@qq.com>
2025-08-16 13:13:29 +0800

    第二次部署

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   // base 设置成你的github仓库名
-  base: '/vitepress-demo1/',
+  base: '/vitepress-demo99/',
   title: "My Awesome Project",
   description: "A VitePress Site",
   themeConfig: {
